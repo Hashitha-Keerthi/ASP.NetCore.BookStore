7f2b77233c23c8bea8baa9670b08c5f44617234d
Hashitha Keerthi <hashitha.18@itfac.mrt.ac.lk>
2021-02-11 11:45:26 +0530

    Added mvc to application

---
+++ HKSolutions.BookStore/HKSolutions.BookStore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HKSolutions.BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+
+        {
+            return "HKSolutions";
+        }
+    }
+}
